ce927f55060daf229d4c1d412862900c1621a720
Guy Golan <guygolan91@gmail.com>
2015-10-04 09:48:33 +0300

    code cosmetics.
    added combo menus.

--- MVP/src/GuiView/Maze2D.java
+++ MVP/src/GuiView/Maze2D.java
@@ -1,8 +1,5 @@
 package GuiView;
 
-import java.util.ArrayList;
-
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
@@ -11,177 +8,168 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 
 import algorithms.mazeGenerators.Position;
-import algorithms.search.State;
 
-public class Maze2D extends MazeDisplayer{
+public class Maze2D extends MazeDisplayer {
 	char crossSection;
 	protected int state;
 	protected int[] goal2d;
-	
-	public Maze2D(Composite parent,int style){
-	       super(parent, style);
-	       state = 1;
-	       crossSection = 'x'; //default
-	       // set a white background   (red, green, blue)
-	       //setBackground(new Color(null, 0, 0, 0));
-	       addPaintListener(new PaintListener() {
-	    		
-				
-				@Override
-				public void paintControl(PaintEvent e) {
-					
-						
-					//System.out.println("Maze2D paintControl");
-					e.gc.setForeground(new Color(null,10,36,106));
-					e.gc.setBackground(new Color(null,10,36,106));
 
-					int width=getSize().x;
-					int height=getSize().y;
-					if ((mazeData!=null)&&(charPosition!=null))
+	public Maze2D(Composite parent, int style) {
+		super(parent, style);
+		state = 1;
+		crossSection = 'x'; // default
+		// set a white background (red, green, blue)
+		// setBackground(new Color(null, 0, 0, 0));
+		addPaintListener(new PaintListener() {
+
+			@Override
+			public void paintControl(PaintEvent e) {
+
+				// System.out.println("Maze2D paintControl");
+				e.gc.setForeground(new Color(null, 10, 36, 106));
+				e.gc.setBackground(new Color(null, 10, 36, 106));
+
+				int width = getSize().x;
+				int height = getSize().y;
+				if ((mazeData != null) && (charPosition != null)) {
+					Image image;
+
+					int imageWidth;
+					int imageHeight;
+					int resizeWidth = getSize().x;
+					int resizeHeight = getSize().y;
+					if (charPosition.equals(mazeData.getExit())) {
+						image = new Image(getDisplay(), "resources/winner.jpg");
+						imageWidth = image.getBounds().width;
+						imageHeight = image.getBounds().height;
+						e.gc.drawImage(image, 0, 0, imageWidth, imageHeight, 0, 0, resizeWidth, resizeHeight);
+
+					} 
+					else 
 					{
-						Image image;
-						
-						int imageWidth ;
-						int imageHeight ;
-						int resizeWidth = getSize().x;
-						int resizeHeight = getSize().y;
-						if (charPosition.equals(mazeData.getExit()))
-						{
-							 image= new Image(getDisplay(),"resources/winner.jpg");
-							 imageWidth = image.getBounds().width;
-							 imageHeight = image.getBounds().height;
-		        		 e.gc.drawImage(image,0,0,imageWidth,imageHeight,0,0,resizeWidth,resizeHeight);
-							
-						}
-						else
-						{
 						setBackground(new Color(null, 0, 0, 0));
 
 						int[][] maze2d = null;
 						int[] position2d = null;
-						switch (crossSection)
-				    	{
-					    	case 'x':
-					    		maze2d = mazeData.getCrossSectionByX(charPosition.getX());
-					    		position2d = new int [] {charPosition.getZ(),charPosition.getY()};
-					    		goal2d = new int [] {mazeData.getExit().getZ(),mazeData.getExit().getY()};
-					    		break;
-					    	case 'y':
-					    		maze2d = mazeData.getCrossSectionByY(charPosition.getY());
-					    		position2d = new int [] {charPosition.getZ(),charPosition.getX()};
-					    		goal2d = new int [] {mazeData.getExit().getZ(),mazeData.getExit().getX()};
-					    		break;
-					    	case 'z':
-					    		maze2d = mazeData.getCrossSectionByZ(charPosition.getZ());
-					    		position2d = new int [] {charPosition.getY(),charPosition.getX()};
-					    		goal2d = new int [] {mazeData.getExit().getY(),mazeData.getExit().getX()};
-					    		break;
-					    	default:
-					    				//TODO
-				    	}
-			    		
-						int w=width/maze2d[0].length;
-						int h=height/maze2d.length;
-	
-						for(int i=0;i<maze2d.length;i++){
-							for(int j=0;j<maze2d[i].length;j++){
-								int x=j*w;
-						        int y=i*h;
-						        if(maze2d[i][j]!=0)
-						        	e.gc.fillRectangle(x,y,w,h);
-						        else if(solution!=null)
-						        {
-						        	int flag = 0;
-						        	
-						        	switch(crossSection)
-						        	{
-							        	case 'x':
-								        	if(solution.contains(new Position(charPosition.getX(),i,j)))
-								        		 flag = 1 ;
-								        	break;
-							        	case 'y':
-							        		if(solution.contains(new Position(i,charPosition.getY(),j))){
-							        			flag = 1;
-							        		}
-							        		break;
-							        	case 'z':
-							        		if(solution.contains(new Position(i,j,charPosition.getZ()))){
-							        			flag =1;
-							        		}
-							        		break;
-						        	}	
-						        	if(flag == 1)
-						        	{
-						        		System.out.println("solution print");
-						        		e.gc.setBackground(new Color(null,255,255,255));
-						        		e.gc.fillOval(x+w/2, y+h/2, w/4, h/4);//  (i, j, w, h);
-						        		//e.gc.fillRectangle(x, y, 8, 8);
-						        		e.gc.setBackground(new Color(null,10,36,106));
-						        	}
-						        }
-						        	
-						    }
+						switch (crossSection) {
+						case 'x':
+							maze2d = mazeData.getCrossSectionByX(charPosition.getX());
+							position2d = new int[] { charPosition.getZ(), charPosition.getY() };
+							goal2d = new int[] { mazeData.getExit().getZ(), mazeData.getExit().getY() };
+							break;
+						case 'y':
+							maze2d = mazeData.getCrossSectionByY(charPosition.getY());
+							position2d = new int[] { charPosition.getZ(), charPosition.getX() };
+							goal2d = new int[] { mazeData.getExit().getZ(), mazeData.getExit().getX() };
+							break;
+						case 'z':
+							maze2d = mazeData.getCrossSectionByZ(charPosition.getZ());
+							position2d = new int[] { charPosition.getY(), charPosition.getX() };
+							goal2d = new int[] { mazeData.getExit().getY(), mazeData.getExit().getX() };
+							break;
+						default:
+							// TODO
 						}
-						int flag =0;
+
+						int w = width / maze2d[0].length;
+						int h = height / maze2d.length;
+
+						for (int i = 0; i < maze2d.length; i++) {
+							for (int j = 0; j < maze2d[i].length; j++) {
+								int x = j * w;
+								int y = i * h;
+								if (maze2d[i][j] != 0)
+									e.gc.fillRectangle(x, y, w, h);
+								else if (solution != null) {
+									int flag = 0;
+
+									switch (crossSection) {
+									case 'x':
+										if (solution.contains(new Position(charPosition.getX(), i, j))) {
+											flag = 1;
+										}
+										break;
+									case 'y':
+										if (solution.contains(new Position(i, charPosition.getY(), j))) {
+											flag = 1;
+										}
+										break;
+									case 'z':
+										if (solution.contains(new Position(i, j, charPosition.getZ()))) {
+											flag = 1;
+										}
+										break;
+									}
+									if (flag == 1) {
+										System.out.println("solution print");
+										e.gc.setBackground(new Color(null, 255, 255, 255));
+										e.gc.fillOval(x + w / 2, y + h / 2, w / 4, h / 4);// (i,
+																							// j,
+																							// w,
+																							// h);
+										// e.gc.fillRectangle(x, y, 8, 8);
+										e.gc.setBackground(new Color(null, 10, 36, 106));
+									}
+								}
+
+							}
+						}
+						int flag = 0;
 						resizeWidth = w;
 						resizeHeight = h;
-						switch(crossSection)
-			        	{
-				        	case 'x':
-				        		if (mazeData.getExit().getX()==charPosition.getX())
-				        			flag = 1 ;
-				        		break;
-				        	case 'y':
-				        		if (mazeData.getExit().getY()==charPosition.getY())	
-				        			flag = 1;
-				        		break;
-				        	case 'z':
-				        		if (mazeData.getExit().getZ()==charPosition.getZ())	
-				        			flag =1;
-				        		break;
-			        	}	
-			        	if(flag == 1)
-			        	{
-			        		 image= new Image(getDisplay(),"resources/pacmanwoman.png");
-								 imageWidth = image.getBounds().width;
-								 imageHeight = image.getBounds().height;
-			        		 e.gc.drawImage(image,0,0,imageWidth,imageHeight,goal2d[0]*w,goal2d[1]*h,resizeWidth,resizeHeight);
-							
-			        	}
-						
-						
-						if (state == 1){
-						 image= new Image(getDisplay(),"resources/pacman.png");
-						state=0;
+						switch (crossSection) {
+						case 'x':
+							if (mazeData.getExit().getX() == charPosition.getX())
+								flag = 1;
+							break;
+						case 'y':
+							if (mazeData.getExit().getY() == charPosition.getY())
+								flag = 1;
+							break;
+						case 'z':
+							if (mazeData.getExit().getZ() == charPosition.getZ())
+								flag = 1;
+							break;
 						}
-						else 
-						{
-						 image = new Image(getDisplay(),"resources/closedpacman.png");
-							state=1;
+						if (flag == 1) {
+							image = new Image(getDisplay(), "resources/pacmanwoman.png");
+							imageWidth = image.getBounds().width;
+							imageHeight = image.getBounds().height;
+							e.gc.drawImage(image, 0, 0, imageWidth, imageHeight, goal2d[0] * w, goal2d[1] * h,
+									resizeWidth, resizeHeight);
+
+						}
+
+						if (state == 1) {
+							image = new Image(getDisplay(), "resources/pacman.png");
+							state = 0;
+						} else {
+							image = new Image(getDisplay(), "resources/closedpacman.png");
+							state = 1;
 						}
 						imageWidth = image.getBounds().width;
-						 imageHeight = image.getBounds().height;
-					 
-						e.gc.drawImage(image,0,0,imageWidth,imageHeight,position2d[0]*w,position2d[1]*h,resizeWidth,resizeHeight);
-						}
+						imageHeight = image.getBounds().height;
+
+						e.gc.drawImage(image, 0, 0, imageWidth, imageHeight, position2d[0] * w, position2d[1] * h,
+								resizeWidth, resizeHeight);
 					}
 				}
-			});
-	 }
+			}
+		});
+	}
 
 	public void setCrossSection(char cross) {
-		if ((cross== 'x')||(cross== 'X')||(cross== 'y')||(cross== 'Y')||(cross== 'z')||(cross== 'Z'))
-		{
-			this.crossSection=cross;
+		if ((cross == 'x') || (cross == 'X') || (cross == 'y') || (cross == 'Y') || (cross == 'z') || (cross == 'Z')) {
+			this.crossSection = cross;
 			Display.getDefault().syncExec(new Runnable() {
-			    public void run() {
-			    	redraw();
-			    	//update();
-			    }
+				public void run() {
+					redraw();
+					// update();
+				}
 			});
 		}
-		//TODO add exeption
-		
+		// TODO add exeption
+
 	}
 
 }
--- MVP/src/GuiView/Maze3D.java
+++ MVP/src/GuiView/Maze3D.java
@@ -3,11 +3,15 @@ package GuiView;
 import java.util.ArrayList;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
@@ -17,88 +21,91 @@ import algorithms.mazeGenerators.Position;
 import algorithms.search.Solution;
 
 public class Maze3D extends MazeDisplayer {
-	
-	protected Maze2D rightDisplay;
+
 	protected Maze2D leftDisplay;
+	protected Maze2D rightDisplay;
 
 	public Maze3D(Composite parent, int style) {
 		super(parent, style);
-		//setBackground(new Color(null, 255, 255, 255));
-		setLayout(new GridLayout(2,true));
-		
+		// setBackground(new Color(null, 255, 255, 255));
+		setLayout(new GridLayout(2, true));
+
 		Label title = new Label(this, SWT.TITLE);
-		title.setText("3d maze display by cross sections");
+		title.setText("3D maze display by cross sections");
 		title.setLayoutData(new GridData(SWT.TOP, SWT.TOP, false, false, 2, 1));
-		
-		rightDisplay = new Maze2D(this, SWT.NULL);
-		rightDisplay.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		rightDisplay.setCrossSection('x');
+
+		Combo leftDisplayOptions = new Combo(this, SWT.DROP_DOWN);
+		leftDisplayOptions.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		leftDisplayOptions.setItems(new String[] { "x", "y", "z" });
+		leftDisplayOptions.select(0);
+		leftDisplayOptions.setBackground(new Color(getDisplay(), 255, 255, 255));
+
+		Combo rightDisplayOptions = new Combo(this, SWT.DROP_DOWN);
+		rightDisplayOptions.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
+		rightDisplayOptions.setItems(new String[] { "x", "y", "z" });
+		rightDisplayOptions.select(1);
+		rightDisplayOptions.setBackground(new Color(getDisplay(), 255, 255, 255));
+
 		leftDisplay = new Maze2D(this, SWT.NULL);
 		leftDisplay.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		leftDisplay.setCrossSection('y');
-		
-		
-		
+		leftDisplay.setCrossSection(leftDisplayOptions.getText().charAt(0));
+		rightDisplay = new Maze2D(this, SWT.NULL);
+		rightDisplay.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		rightDisplay.setCrossSection(rightDisplayOptions.getText().charAt(0));
+
 		addPaintListener(new PaintListener() {
-			
+
 			@Override
 			public void paintControl(PaintEvent e) {
-				
+
 			}
-			
+
 		});
-	
+
 	}
+
 	@Override
-	public void setMazeData(Maze3d maze)
-	{
+	public void setMazeData(Maze3d maze) {
 		this.mazeData = maze;
 		leftDisplay.setMazeData(maze);
 		rightDisplay.setMazeData(maze);
 		widgetsRefresh();
-		
+
 	}
 
 	@Override
-	public void addKeyListener(org.eclipse.swt.events.KeyListener listener) {
+	public void addKeyListener(KeyListener listener) {
 		super.addKeyListener(listener);
 		leftDisplay.addKeyListener(listener);
 		rightDisplay.addKeyListener(listener);
 	}
 
 	@Override
-	public void setCharPosition(Position position)
-	{
+	public void setCharPosition(Position position) {
 		this.charPosition = position;
 		leftDisplay.setCharPosition(position);
 		rightDisplay.setCharPosition(position);
 		widgetsRefresh();
-		
+
 	}
+
 	@Override
-	public void setSolution(ArrayList<Position> solution)
-	{
+	public void setSolution(ArrayList<Position> solution) {
 		this.solution = solution;
 		leftDisplay.setSolution(solution);
-		rightDisplay.setSolution( solution);
+		rightDisplay.setSolution(solution);
 		widgetsRefresh();
-		
+
 	}
-	public void widgetsRefresh()
-	{
-			if((mazeData !=null)&&(charPosition!=null))
-				Display.getDefault().syncExec(new Runnable() {
-				    public void run() {
-				    	
-				    	redraw();
-				    }
-				});
-			
+
+	public void widgetsRefresh() {
+		if ((mazeData != null) && (charPosition != null))
+			Display.getDefault().syncExec(new Runnable() {
+				public void run() {
+
+					redraw();
+				}
+			});
+
 	}
 }
-	
-	
-	
-	
-
-
