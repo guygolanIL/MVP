b12466947a6b67774780db8df03939c340aea5b3
Guy Golan <guygolan91@gmail.com>
2015-10-09 09:30:43 +0300

    added javadoc for the main

--- MVP/src/boot/MVPTest.java
+++ MVP/src/boot/MVPTest.java
@@ -17,42 +17,42 @@ import view.ObservableCommonView;
 public class MVPTest {
 
 	public static void main(String[] args) {
-	String defaultXMLname = "properties.xml";
-				Properties prop;
-				try {
-					FileInputStream in = new FileInputStream(defaultXMLname);
-					XMLDecoder decoder = new XMLDecoder(in);
-					 prop = (Properties)decoder.readObject();
-					decoder.close();
+		String defaultXMLname = "properties.xml";
+		Properties prop;
+		try {
+			FileInputStream in = new FileInputStream(defaultXMLname);		//tries to read the properties.xml default pathname.
+			XMLDecoder decoder = new XMLDecoder(in);
+			prop = (Properties)decoder.readObject();		//decoding the xml file.
+			decoder.close();
 					
-				} catch (FileNotFoundException e) {
-					System.out.println("file not found, default properties will be loaded");
-					prop = new Properties();
-					prop.setDefaults();
-				}
-				ObservableCommonView view = null;
-				switch (prop.getUi())
-				{
-					case "Command line":
-						view = new MyObservableCLIView(new BufferedReader(new InputStreamReader(System.in)) , new PrintWriter(System.out));
-						break;
-					case "Graphic user interface":
-						view = new MyObservableGuiView("maze game", 800, 500);
-						break;
-					default:
-						view = new MyObservableGuiView("maze game", 800, 500);	
-				}
+		} catch (FileNotFoundException e) {				//if no properties.xml was found in directory, generating default properties.
+			System.out.println("file not found, default properties will be loaded");
+			prop = new Properties();
+			prop.setDefaults();
+		}
+		ObservableCommonView view = null;
+		switch (prop.getUi())				//according to the properties initializing the view.
+		{
+			case "Command line":
+				view = new MyObservableCLIView(new BufferedReader(new InputStreamReader(System.in)) , new PrintWriter(System.out));
+				break;
+			case "Graphic user interface":
+				view = new MyObservableGuiView("maze game", 800, 500);
+				break;
+			default:
+				view = new MyObservableGuiView("maze game", 800, 500);	
+		}
 				
-				MyObservableModel model = new MyObservableModel();
-				Presenter p = new Presenter(model,view);
+		MyObservableModel model = new MyObservableModel();
+		Presenter p = new Presenter(model,view);		
 				
-				p.setProperties(prop);
-				p.setDebugMode(true);
+		p.setProperties(prop);		//setting the properties of the system for the presenter.
+		p.setDebugMode(true);		//debug mode turned on.
 				
-				model.addObserver(p);
-				view.addObserver(p);
+		model.addObserver(p);		//presenter observing the model for changes.
+		view.addObserver(p);		//presenter observing the view for changes.
 				
-				view.start();
-			}
+		view.start();				//start of the user interface.
+	}
 
 }
